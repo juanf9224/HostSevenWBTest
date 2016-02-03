b01a7a891f78aa893d675a0abf5ec30174526307
JBG INC <juanfh.24.6@gmail.com>
2016-02-02 22:23:16 -0400

    Partners Buttons Layouts

--- app/src/main/java/com/hostseven/hostsevenwbtest/Main3Activity.java
+++ app/src/main/java/com/hostseven/hostsevenwbtest/Main3Activity.java
@@ -37,10 +37,12 @@ public class Main3Activity extends AppCompatActivity {
 
     TextView userId, userNum, userName;
     String day;
-    ListView list;
+    ListView list, list2;
     ArrayList<HashMap<String, String>> mylist = new ArrayList<HashMap<String, String>>();
     HashMap<String, String> map = new HashMap<String, String>();
     ProgressDialog progress;
+    ArrayList<HashMap<String, String>> mylistDef = new ArrayList<HashMap<String, String>>();
+    HashMap<String, String> mapDef = new HashMap<String, String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,19 +57,18 @@ public class Main3Activity extends AppCompatActivity {
         userName =(TextView)findViewById(R.id.userName);
 
 
-        /*ArrayList<HashMap<String, String>> mylistDef = new ArrayList<HashMap<String, String>>();
-        HashMap<String, String> mapDef = new HashMap<String, String>();
-
-        mapDef.put("Grupo", "Grupo");
-        mapDef.put("Ejercicio", "Ejercicio");
-        mapDef.put("Series", "Series");
-        mapDef.put("Repeticiones", "Repeticiones");
-        mylistDef.add(map);
 
 
-        list = (ListView)findViewById(R.id.listView2);
-        SimpleAdapter mAdapter1 = new SimpleAdapter(this, mylistDef, R.layout.row, new String[] {"Grupo", "Ejercicio", "Series", "Repeticiones"}, new int[] {R.id.routines_group, R.id.routines_exercises, R.id.routines_series, R.id.routines_repeat});
-        list.setAdapter(mAdapter1);*/
+        map.put("Grupo", "Grupo");
+        map.put("Ejercicio", "Ejercicio");
+        map.put("Series", "Series");
+        map.put("Repeticiones", "Repeticiones");
+        mylist.add(map);
+
+
+        list = (ListView)findViewById(R.id.listView);
+        SimpleAdapter mAdapter1 = new SimpleAdapter(this, mylist, R.layout.row, new String[] {"Grupo", "Ejercicio", "Series", "Repeticiones"}, new int[] {R.id.routines_group, R.id.routines_exercises, R.id.routines_series, R.id.routines_repeat});
+        list.setAdapter(mAdapter1);
 
         userId.setText(bundle.getString("userId"));
         userNum.setText(bundle.getString("userNum"));
